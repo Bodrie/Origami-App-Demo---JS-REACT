13da0e598035a2097410f17b56608ab777f4926e
Dobromir Kirov <80153626+Bodrie@users.noreply.github.com>
2021-03-24 11:13:34 +0200

    Adding Main container

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import Main from './components/Main/Main';
+import { Component } from 'react';
 import style from './App.module.css';
 
-function App() {
-  return (
-    <div className={style.app}>
-      
-    <Header />
+//function App() {
+//    return (
+//        <div className={style.app}>
+//            <Header />
+//
+//            <div className={style.container}>
+//                <Menu />
+//                <Main />
+//            </div>
+//        </div>
+//    );
+//}   
 
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className={style.app}>
+            <Header />
+
+            <div className={style.container}>
+                <Menu />
+                <Main />
+            </div>
+        </div>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import './Main.css';
+
+const Main = () => {
+    return (
+        <main className="main-container">
+            <h1>Soooome heading...</h1>
+            
+            <p>Some posts...</p>
+        </main>
+    );
+}
+
+export default Main;
